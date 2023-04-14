53daa7eb16a6fbb7a6de30b8c15c6b3ab21a122c
IrinaMoroz <moroz.irina3373@gmail.com>
2023-04-14 21:19:26 +0300

    Создана главная страница и меню с добавлением статей и тестов

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+    include "mysql.php";
+    //phpinfo();
+
+    //$themes = $conn->query("SELECT * FROM themes")->fetch_all(MYSQLI_ASSOC);
+
+    // вывод главной страницы
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>Главная</title>
+    </head>
+    <body>
+        <div>
+            <a href = "table.php"> Статьи Ветхий Завет</a>
+        </div>
+    </body>
+    </html>
